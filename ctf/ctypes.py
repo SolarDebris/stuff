dc3554e1300e29766b598c9242417cfae29b0267
Alex Schmith <alexanderschmith@protonmail.com>
2023-10-08 17:19:58 -0400

    Update ctypes.py

--- ctf/ctypes.py
+++ ctf/ctypes.py
@@ -4,3 +4,13 @@ from ctypes import *
 libc = CDLL("./libc.so.6")
 rand = libc.rand()
 srand = libc.srand()
+
+
+def seed_srand(seed):
+    srand(seed)
+
+def generate_rand(num):
+    rand_nums = []
+    for i in range(num):
+        rand_nums.append(rand())
+    return rand_nums
