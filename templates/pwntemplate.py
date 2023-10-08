b74c8b6f608b48612abc2d16c17709c50b6c5336
Alex Schmith <alexanderschmith@protonmail.com>
2023-10-08 15:21:51 -0400

    Update pwntemplate.py
    Added heap scaffolding and some lambda functions

--- templates/pwntemplate.py
+++ templates/pwntemplate.py
@@ -1,21 +1,24 @@
 #! /usr/bin/python
-
 from pwn import *
 
 context.update(
         arch="amd64",
-        #arch="i386",
         endian="little",
         log_level="debug",
         os="linux",
         terminal=["tmux", "split-window", "-h", "-p 65"]
         #terminal=["st"]
 )
+to = 2
+
+ru = lambda p,s: p.recvuntil(s, timeout=to)
+rl = lambda p: p.recvline()
+sla = lambda p,a,b: p.sendlineafter(a, b, timeout=to)
+sl = lambda p,a: p.sendline(a)
 
 def start(binary):
 
     gs = '''
-        init-pwndbg
         set context-sections stack regs disasm
         set show-compact-regs on
         set resolve-heap-via-heuristic on
@@ -29,14 +32,39 @@ def start(binary):
     else:
         return process(binary)
 
-def exploit(p,e,r):
-    return None
+def create(p, size, value):
+    ru(p,b"")
+    sl(p,b"%i" % size)
+    ru(p,b"")
+    sl(value)
+
+def edit(p, index, value):
+    ru(p,b"")
+    sl(p,"%i" % index)
+    ru(p,b"")
+    sl(p,value)
+
+def delete(p, index):
+    ru(p,b"")
+    sl(p,"%i" % index)
+
+def view(p, index):
+    ru(p,b"")
+    sl(p,"%i" % index)
+    ru(p,b"")
+    return rl(p)
+    
+
+def exploit(p,e,l):
+
+    p.interactive()
+    
 
 if __name__=="__main__":
     file = args.BIN
 
     p = start(file)
     e = context.binary = ELF(file)
-    r = ROP(e)
+    l = ELF("./libc.so.6")
 
-    exploit(p,e,r)
+    exploit(p,e,l)
