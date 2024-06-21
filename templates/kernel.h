89709f46d4196ab3a9cb03d9c846c4d465fc4eb0
Alex Schmith <alexanderschmith@protonmail.com>
2024-06-21 10:56:32 -0400

    Update kernel.h

--- templates/kernel.h
+++ templates/kernel.h
@@ -34,6 +34,32 @@ uint64_t u64(uint8_t *buf)
     return res;
 }
 
+// See https://www.kernel.org/doc/Documentation/vm/pagemap.txt
+uint64_t virt2phys(void* p)
+{
+    uint64_t virt = (uint64_t)p;
+
+    // Assert page alignment
+    assert((virt & 0xfff) == 0);
+
+    int fd = open("/proc/self/pagemap", O_RDONLY);
+    if (fd == -1)
+        die("open");
+
+    uint64_t offset = (virt / 0x1000) * 8;
+    lseek(fd, offset, SEEK_SET);
+
+    uint64_t phys;
+    if (read(fd, &phys, 8 ) != 8)
+        die("read");
+
+    // Assert page present
+    assert(phys & (1ULL << 63));
+
+    phys = (phys & ((1ULL << 54) - 1)) * 0x1000;
+    return phys;
+}
+
 
 size_t user_cs, user_ss, user_rflags, user_sp;
 void save_status()
